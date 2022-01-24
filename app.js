1beaeed030602ae84623d4b29b7a0794cec5a181
richardj-codes <74265663+richardj-codes@users.noreply.github.com>
2022-01-24 08:55:12 -0600

    added initial js

--- app.js
+++ app.js
@@ -1,10 +1,153 @@
 let hamburger = document.querySelector(".menu__hamburger");
 let menuList = document.querySelector(".menu__list");
+let locationInput = document.querySelector("#location-input");
+let city = document.querySelector(".weather-today__city");
+let temp = document.querySelector(".weather-today__temperature");
+let extrasFeels = document.querySelector(".feels-like");
+let extrasHigh = document.querySelector(".high-temp");
+let extrasLow = document.querySelector(".low-temp");
+const geoMarker = document.querySelector(".fa-map-marker-alt");
+// just wanted to use a random object... because I was bored
+const api = {
+	apiKey: "74a6a74158e6ab0a7c7537bafef62835",
+};
 
 hamburger.addEventListener("click", () => {
 	menuList.classList.toggle("open");
+	// menuList.classList.toggle("open");
 
 	console.log("hamburger is clicked");
 });
 
-console.log("kjgkhgjh");
+locationInput.addEventListener("keydown", function (e) {
+	let apiCity;
+	if (e.keyCode == 13) {
+		//keyCode for enter
+		e.preventDefault();
+		apiCity = locationInput.value;
+		apiCitySplit = apiCity.split(" ");
+		inputApi(
+			apiCitySplit
+				.map((cityUpper) => {
+					return (
+						cityUpper[0].toUpperCase() + cityUpper.substring(1).toLowerCase()
+					);
+				})
+				.join(" ")
+		);
+
+		// apiCity = cityApiCall();
+		// inputApi(apiCity);
+		// console.log(apiCity);
+	}
+});
+
+let inputApi = async (cityValue) => {
+	try {
+		let response = await fetch(
+			`http://api.openweathermap.org/data/2.5/weather?q=${cityValue}&units=imperial&appid=${api.apiKey}`
+		);
+		let data = await response.json();
+		// console.log(data.main);
+		mainTemp(cityValue, Math.round(data.main.temp));
+		extras(
+			Math.round(data.main.feels_like),
+			Math.round(data.main.temp_max),
+			Math.round(data.main.temp_min)
+		);
+	} catch (e) {
+		// throw new Error(e);
+		console.log(e, ":there's a mistake somewhere!!!");
+	}
+};
+
+// seeing if i can make individual functions for each element, just for fun (and practice)
+
+let mainTemp = (cityName, mainTemp) => {
+	// city.textContent = cityName;
+	city.textContent = cityName;
+	// city.textContent = `yoooooo`;
+	temp.textContent = mainTemp;
+};
+
+let extras = (feelsLike, highTemps, lowTemps) => {
+	extrasFeels.textContent = feelsLike;
+	extrasHigh.textContent = highTemps;
+	extrasLow.textContent = lowTemps;
+};
+
+geoMarker.addEventListener("click", () => {
+	navigator.geolocation.getCurrentPosition(successCall, errorCall, {
+		enableHighAccuracy: true,
+	});
+});
+let geoBtnApi = async (cityGeoUpdateParam, lat, long) => {
+	try {
+		let response = await fetch(
+			`http://api.openweathermap.org/data/2.5/weather?q=${cityGeoUpdateParam}&units=imperial&appid=${api.apiKey}`
+
+			// `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=minutely,alerts,hourly&appid=${api.apiKey}`
+		);
+		let data = await response.json();
+		mainTemp(cityGeoUpdateParam, Math.round(data.main.temp));
+		extras(
+			Math.round(data.main.feels_like),
+			Math.round(data.main.temp_max),
+			Math.round(data.main.temp_min)
+		);
+	} catch (e) {
+		console.log(e, ":there's a mistake somewhere!!!");
+	}
+};
+
+const successCall = (position) => {
+	let lat = position.coords.latitude;
+	let long = position.coords.longitude;
+	console.log(lat, long);
+	// apiCity = locationInput.value;
+
+	cityApiCall(lat, long);
+};
+const errorCall = () => {
+	throw new Error(console.log(`It's a no for me dawg"`));
+};
+
+let cityApiCall = async (lat, long) => {
+	try {
+		let response = await fetch(
+			`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=en`
+		);
+
+		let cityApiCallData = await response.json();
+		// console.log(cityApiCallData);
+		let cityGeoUpdate = city.textContent;
+		cityGeoUpdate = cityApiCallData.locality;
+		// let cityApiCallDataName = cityApiCallData.city;
+		// console.log(cityApiCallData.city);
+		// console.log(cityGeoUpdate);
+		// city.textContent = cityGeoUpdate;
+		geoBtnApi(cityGeoUpdate, lat, long);
+		// console.log(mainTemp(cityGeoUpdate));
+		// console.log(mainTemp);
+	} catch (e) {
+		throw new Error(console.log(e));
+	}
+};
+// import axios
+// research axios
+// research how to separate city and state into two variables
+//
+
+// auto update the weather based on longitude and lattitude button click
+// update weather based on city, state, zipcode and country code
+
+// activate the enter key
+
+//
+
+// console.log(weatherApi("cleveland"));
+
+// console.log(response);
+// document.addEventListener("DOMContentLoaded", )
+
+// API to populate city name
